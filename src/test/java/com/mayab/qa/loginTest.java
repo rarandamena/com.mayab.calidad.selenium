a4bb31da5b9efa151b7bc344b2e5c7b0a925dc16
rafael <rarandamena@gmail.com>
2019-04-02 19:44:24 -0600

    ahora si, implementado usar el cuadro de mensaje de contraseña incorrecta cuando el login es fallido y agregada la comprobación del login correcto a través de ver que el nombre de usuario coincida con el de la persona que queremos usar en la barra de facebook.

--- src/test/java/com/mayab/qa/loginTest.java
+++ src/test/java/com/mayab/qa/loginTest.java
@@ -1,5 +1,6 @@
 package com.mayab.qa;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.*;
@@ -9,6 +10,7 @@ import org.openqa.selenium.interactions.Pause;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class loginTest {
@@ -33,12 +35,29 @@ public class loginTest {
     public void JavaLoginSuccess() throws InterruptedException {
         driver.get(URL);
         WebElement username = driver.findElement(By.xpath("//*[@id=\"email\"]"));
+        username.sendKeys("rafaelarandamena@gmail.com");
+        WebElement pass = driver.findElement(By.xpath("//*[@id=\"pass\"]"));
+        pass.sendKeys("cartas10");
+        pass.submit();
+        Thread.sleep(3000);
+        WebElement passOK = driver.findElement(By.xpath("//*[@id=\"u_0_a\"]/div[1]/div[1]/div/a/span/span"));
+        String loginSuccessful = passOK.getText();
+        assertEquals(loginSuccessful, "Rafaela");
+    }
+    @Test
+    public void JavaLoginFailed() throws InterruptedException {
+        driver.get(URL);
+        WebElement username = driver.findElement(By.xpath("//*[@id=\"email\"]"));
         username.sendKeys("9981056448");
         WebElement pass = driver.findElement(By.xpath("//*[@id=\"pass\"]"));
-        pass.sendKeys("kerberos1#");
+        pass.sendKeys("kerberos1987#");
         pass.submit();
 
         Thread.sleep(3000);
+        WebElement errorPass = driver.findElement(By.xpath("//*[@id=\"globalContainer\"]/div[3]/div/div/div"));
+        String loginFail =errorPass.getText();
+        Assert.assertEquals(loginFail, "La contraseña que ingresaste es incorrecta. ¿Olvidaste tu contraseña?" );
 
     }
+    //
 }
